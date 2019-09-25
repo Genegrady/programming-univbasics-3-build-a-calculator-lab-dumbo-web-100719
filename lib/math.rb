2bb40bf4fb6c2199eebd303c3079253bed553cab
Eugene Grady <grady.gene.thomas@gmail.com>
2019-09-25 15:42:55 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,19 +4,23 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  sub = num1 - num2
+  return sub
 end
 
 def division(num1, num2)
-
+  div = num1/num2
+  return div
 end
 
 def multiplication(num1, num2)
-
+  mult = num1 * num2
+  return mult
 end
 
 def modulo(num1, num2)
-
+  mod = num1 % num2
+  return mod
 end
 
 def square_root(num)
